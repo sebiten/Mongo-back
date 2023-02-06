72e83f026fbe99223a6d8a409acefc70445951e1
Sebiten <sebdevspace@gmail.com>
2023-02-06 12:20:45 -0300

    password hashed

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,17 +1,23 @@
 import Usuario from "../models/Usuario.js";
 
 const registrar = async (req, res) => {
-  
+  // evitar reg duplicados
+
+  const { email } = req.body;
+  const existeUsuario = await Usuario.findOne({
+    email,
+  });
+  if (existeUsuario) {
+    const error = new Error("Usuario ya registrado");
+    return res.status(400).json({ msg: error.message });
+  }
+
   try {
     const usuario = new Usuario(req.body);
     const usuarioAlmacenado = await usuario.save();
-    res.json(usuarioAlmacenado)
-
+    res.json(usuarioAlmacenado);
   } catch (error) {
     console.log(error);
   }
-  
-
-
-}
+};
 export { registrar };
