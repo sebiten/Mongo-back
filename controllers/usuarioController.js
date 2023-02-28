c8018b46f9089a060795e1265f4f2429078238fb
Sebiten <sebdevspace@gmail.com>
2023-02-28 09:42:50 -0300

    forgot password

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -2,7 +2,6 @@ import Usuario from "../models/Usuario.js";
 import gererarId from "../helpers/generarId.js";
 import generarJWT from "../helpers/generarJWT.js";
 
-
 const registrar = async (req, res) => {
   // evitar reg duplicados
 
@@ -26,33 +25,75 @@ const registrar = async (req, res) => {
 };
 
 const autenticar = async (req, res) => {
-
-  const {email, password} = req.body
- // Comprobar si el usurio existe
-  const usuario = await Usuario.findOne({email});
+  const { email, password } = req.body;
+  // Comprobar si el usurio existe
+  const usuario = await Usuario.findOne({ email });
   if (!usuario) {
     const error = new Error("El usuario no existe");
-    return res.status(404).json({ msg: error.message })
+    return res.status(404).json({ msg: error.message });
   }
- // comprobar si esta confirmado
+  // comprobar si esta confirmado
   if (!usuario.confirmado) {
     const error = new Error("El usuario no esta confirmado");
-    return res.status(403).json({ msg: error.message})
+    return res.status(403).json({ msg: error.message });
   }
   // comprobar el password
   if (!usuario.comprobarPassword(password)) {
     const error = new Error("El password es incorrecto");
-    return res.status(403).json({ msg: error.message })
+    return res.status(403).json({ msg: error.message });
   }
   // generar el JWT
   const token = generarJWT(usuario._id);
-  res.json(
-    {
-      _id: usuario._id,
-      nombre: usuario.nombre,
-      email: usuario.email,
-      token
-    }
-  )
+  res.json({
+    _id: usuario._id,
+    nombre: usuario.nombre,
+    email: usuario.email,
+    token,
+  });
 };
-export { registrar, autenticar };
+
+const confirmar = async (req, res) => {
+  const { token } = req.params;
+  const usuarioConfirmado = await Usuario.findOne({ token });
+  if (!usuarioConfirmado) {
+    const error = new Error("El usuario no existe");
+    return res.status(404).json({ msg: error.message });
+  }
+  try {
+    usuarioConfirmado.confirmado = true;
+    usuarioConfirmado.token = "";
+    await usuarioConfirmado.save();
+    res.json({ msg: "Usuario confirmado" });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const olvidePassword = async (req, res) => {
+  const { email } = req.body;
+  const usuario = await Usuario.findOne({ email });
+  if (!usuario) {
+    const error = new Error("El usuario no existe");
+    return res.status(404).json({ msg: error.message });
+  }
+  try {
+    usuario.token = gererarId();
+    await usuario.save();
+    res.json({ msg: "Revisa tu email para cambiar el password" });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const comprobarToken = async (req, res) => {
+  const { token } = req.params;
+
+  const tokenValido = await Usuario.findOne({ token });
+  if (tokenValido) {
+    res.json({ msg: "Token valido y el usuario existe" })
+  } else {
+    const error = new Error("El token no es valido");
+    return res.status(404).json({ msg: error.message });
+  }
+};
+export { registrar, autenticar, confirmar, olvidePassword, comprobarToken };
--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -1,9 +1,17 @@
-import express  from "express";
+import express from "express";
 const router = express.Router();
-import { registrar, autenticar } from '../controllers/usuarioController.js'
+import {
+  registrar,
+  autenticar,
+  confirmar,
+  olvidePassword,
+  comprobarToken,
+} from "../controllers/usuarioController.js";
 
 // creacion de registro y confimacion de usuarios
-router.post("/", registrar) // crea un nuevo usuario
-router.post("/login",autenticar)
-
+router.post("/", registrar); // crea un nuevo usuario
+router.post("/login", autenticar);
+router.get("/confirmar/:token", confirmar);
+router.post("/olvide-password", olvidePassword);
+router.get("/olvide-password/:token", comprobarToken);
 export default router;
